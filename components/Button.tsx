5cb0fbc865f680cd9c57efda0ada3338d4ce752d
Yakshit chhipa <yakshitchhipa@gmail.com>
2024-03-19 22:01:54 +0530

    feat: button

---
+++ components/Button.tsx
@@ -0,0 +1,21 @@
+import clsx from 'clsx';
+import React from 'react';
+
+interface ButtonProps extends React.ComponentProps<"button"> {
+	text: string,
+	varient: "outline" | "filled",
+	className?: string,
+}
+
+export default function Button({ text, varient, className, ...props }: ButtonProps) {
+
+	switch (varient) {
+		case "filled":
+			return <button className={clsx(`rounded-xl py-2 px-4 mx-2 bg-mybg text-col`, className)} {...props}>{text}</button>
+	
+		case "outline":
+			return <button className={clsx("rounded-xl py-2 px-4 mx-2 border border-neutral-700", className)} {...props}>{text}</button>
+	}
+
+
+}
