255f25bf04c06ff7b481454d073e6bce4559f891
Yakshit chhipa <yakshitchhipa@gmail.com>
2024-03-20 07:57:34 +0530

    change: svg

--- components/Stacks.tsx
+++ components/Stacks.tsx
@@ -6,7 +6,7 @@ const sdks = [
     "python",
     "go",
     "java",
-    "rest",
+    "bash",
     "rust",
 ]
 
@@ -21,8 +21,8 @@ export default function Stacks() {
     return (
         <div className="min-h-screen w-full m-auto flex flex-col items-center gap-14 p-8 text-center">
             <div className="flex flex-col items-center justify-center w-2/3">
-                <svg className={`border-2 border-mybg rounded-[2rem] p-4 w-32 h-32 m-8 fill-mybg`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeWidth={1}>
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M14.25 6.087c0-.355.186-.676.401-.959.221-.29.349-.634.349-1.003 0-1.036-1.007-1.875-2.25-1.875s-2.25.84-2.25 1.875c0 .369.128.713.349 1.003.215.283.401.604.401.959v0a.64.64 0 0 1-.657.643 48.39 48.39 0 0 1-4.163-.3c.186 1.613.293 3.25.315 4.907a.656.656 0 0 1-.658.663v0c-.355 0-.676-.186-.959-.401a1.647 1.647 0 0 0-1.003-.349c-1.036 0-1.875 1.007-1.875 2.25s.84 2.25 1.875 2.25c.369 0 .713-.128 1.003-.349.283-.215.604-.401.959-.401v0c.31 0 .555.26.532.57a48.039 48.039 0 0 1-.642 5.056c1.518.19 3.058.309 4.616.354a.64.64 0 0 0 .657-.643v0c0-.355-.186-.676-.401-.959a1.647 1.647 0 0 1-.349-1.003c0-1.035 1.008-1.875 2.25-1.875 1.243 0 2.25.84 2.25 1.875 0 .369-.128.713-.349 1.003-.215.283-.4.604-.4.959v0c0 .333.277.599.61.58a48.1 48.1 0 0 0 5.427-.63 48.05 48.05 0 0 0 .582-4.717.532.532 0 0 0-.533-.57v0c-.355 0-.676.186-.959.401-.29.221-.634.349-1.003.349-1.035 0-1.875-1.007-1.875-2.25s.84-2.25 1.875-2.25c.37 0 .713.128 1.003.349.283.215.604.401.96.401v0a.656.656 0 0 0 .658-.663 48.422 48.422 0 0 0-.37-5.36c-1.886.342-3.81.574-5.766.689a.578.578 0 0 1-.61-.58v0Z" />
+                <svg className={`border-2 border-neutral-700 rounded-[2rem] p-4 w-32 h-32 m-8 stroke-mybg`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeWidth={1.5}>  
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5" />
                 </svg>
                 <h1 className="text-6xl tracking-tight">
                     Integrate Easily
