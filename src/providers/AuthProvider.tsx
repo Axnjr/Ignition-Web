dd41f318e42192d9305012febfcf4cf84f272b0d
Yakshit chhipa <yakshitchhipa@gmail.com>
2024-03-29 10:19:32 +0530

    refractor: layout file name

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,7 +1,5 @@
 "use client"
 import { SessionProvider } from "next-auth/react"
-import { useState } from "react";
-import { getSiteURL } from "@/lib/utils";
 
 export default function AllProvider({ children }: { children: React.ReactNode }) {
 	return (
