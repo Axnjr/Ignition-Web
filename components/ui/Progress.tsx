903e9b10822b0d400845361dec11d97c63aa0feb
Yakshit chhipa <yakshitchhipa@gmail.com>
2024-04-02 10:51:31 +0530

    style: new components

--- components/ui/Progress.tsx
+++ components/ui/Progress.tsx
@@ -18,7 +18,7 @@ const Progress = React.forwardRef<
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-black dark:bg-white transition-all"
+      className="h-full w-full flex-1 bg-mybg transition-all"
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
