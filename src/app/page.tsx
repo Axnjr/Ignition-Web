07212db41cd3ed71bc8b51a3ffffea0bc164af1d
Yakshit chhipa <yakshitchhipa@gmail.com>
2024-04-02 10:53:14 +0530

    style: perf home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,26 +30,27 @@ export default function Home() {
 	// const user  = session?.user?.name;
 	// const email = session?.user?.email
 	return (
-		<div className="bg-[#000000] text-white">
+		<div className="text-white">
 			<Nav />
-			<Menu/>
-			<main className=" flex 
-			flex-col items-start justify-center min-h-screen relative">
-				<div className="-mt-24 min-[1024px]:w-3/4 min-[1024px]:m-auto min-[1024px]:pt-12 h-full flex flex-col items-center justify-center">
-					<p className="text-mybg my-8 bg-white/5 backdrop-blur rounded-3xl md:rounded-4xl
-					flex items-center py-2 px-4 text-xs sm:text-sm text-center">
-						Robust, Performant & litrally real time powered by Rust.
-					</p>
-					{/* bg-clip-text text-transparent bg-gradient-to-tr from-gray-400 to-white */}
+			<Menu />
+			<main className="flex flex-col items-start justify-center absolute py-28 h-fit w-full
+			top-0 z-[-2] bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,#1000f20b,rgba(255,255,255,0))]">
+				<div className="min-[1024px]:w-3/4 min-[1024px]:m-auto min-[1024px]:pt-12 h-full flex flex-col
+				 items-center justify-center">
+					<div className="flex my-8 w-full flex-row items-center justify-center">
+						<button className="animate-border inline-block rounded-3xl bg-white bg-gradient-to-r from-blue-50 via-blue-200 to-blue-400 bg-[length:400%_400%] p-[1px]">
+							<span className="block rounded-3xl px-4 py-1 bg-black backdrop-blur text-xs"> 
+								Robust, Performant & litrally real time powered by Rust.
+							</span>
+						</button>
+					</div>
 					<h1 className="text-[2.5rem] leading-[1] px-6 md:text-6xl min-[1024px]:text-7xl tracking-tight 
-					font-medium text-center">
-						The Ultimate platform
-						for your real time 
-						applications.
+					font-medium text-center ">
+						The Ultimate Real Time Platform.
 					</h1>
-					<span className="text-base sm:text-lg w-[70%] sm:w-2/3 md:w-1/2 text-center font-medium 
+					<span className="text-base sm:text-lg w-[70%] sm:w-2/3 md:w-2/3 text-center font-medium 
 					tracking-wide text-neutral-500 mt-8">
-							We handle all the complexity of the real time infrastructure, so that you get real time for real cheap & real quick!
+						We handle all the complexity of the real time infrastructure, so that you get real time for real cheap & real quick!
 					</span>
 					<br />
 					<div className="flex items-center gap-0 mt-4">
@@ -57,11 +58,8 @@ export default function Home() {
 						<Button text="Contact Us" varient="outline" />
 					</div>
 				</div>
+				<Stacks/>
 			</main>
-			<UseCases/>
-			<Stacks/>
-			<Steps/>
-			<Footer/>
-	  	</div>
+		</div>
 	)
 }
