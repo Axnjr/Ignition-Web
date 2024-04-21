c9823968aef0a47509319e34fe06b44dcc61264c
Yakshit Chhipa <yakshitchhipa@gmail.com>
2024-04-21 10:34:34 +0530

    refractor: evervault

--- components/ui/evervault-card.tsx
+++ components/ui/evervault-card.tsx
@@ -5,10 +5,10 @@ import { useMotionTemplate, motion } from "framer-motion";
 import { cn } from "@/lib/utils";
 
 export const EvervaultCard = ({
-  text,
+  children,
   className,
 }: {
-  text?: string;
+  children?: React.ReactNode;
   className?: string;
 }) => {
   let mouseX = useMotionValue(0);
@@ -39,18 +39,15 @@ export const EvervaultCard = ({
     >
       <div
         onMouseMove={onMouseMove}
-        className="group/card rounded-3xl w-full relative overflow-hidden bg-transparent flex items-center justify-center h-full"
+        className="group/card w-full relative overflow-hidden bg-transparent h-full"
       >
         <CardPattern
           mouseX={mouseX}
           mouseY={mouseY}
           randomString={randomString}
         />
-        <div className="relative z-10 flex items-center justify-center">
-          <div className="relative h-44 w-44  rounded-full flex items-center justify-center text-white font-bold text-4xl">
-            <div className="absolute w-full h-full bg-white/[0.8] dark:bg-black/[0.8] blur-sm rounded-full" />
-            <span className="dark:text-white text-black z-20">{text}</span>
-          </div>
+        <div className="relative h-full z-10 p-8 flex flex-col items-start justify-end">
+              {children}
         </div>
       </div>
     </div>
@@ -58,21 +55,21 @@ export const EvervaultCard = ({
 };
 
 export function CardPattern({ mouseX, mouseY, randomString }: any) {
-  let maskImage = useMotionTemplate`radial-gradient(250px at ${mouseX}px ${mouseY}px, white, transparent)`;
-  let style = { maskImage, WebkitMaskImage: maskImage };
+  // let maskImage = useMotionTemplate`radial-gradient(250px at ${mouseX}px ${mouseY}px, white, transparent)`;
+  // let style = { maskImage, WebkitMaskImage: maskImage };
 
   return (
     <div className="pointer-events-none">
-      <div className="absolute inset-0 rounded-2xl  [mask-image:linear-gradient(white,transparent)] group-hover/card:opacity-50"></div>
+      <div className="absolute inset-0 rounded-2xl [mask-image:linear-gradient(white,transparent)] group-hover/card:opacity-50"></div>
       <motion.div
-        className="absolute inset-0 rounded-2xl bg-gradient-to-r from-green-500 to-blue-700 opacity-0  group-hover/card:opacity-100 backdrop-blur-xl transition duration-500"
-        style={style}
+        className="absolute inset-0 rounded-2xl bg-fancyflipdark opacity-50 backdrop-blur-xl transition duration-500"
+        // style={style}
       />
       <motion.div
-        className="absolute inset-0 rounded-2xl opacity-0 mix-blend-overlay  group-hover/card:opacity-100"
-        style={style}
+        className="absolute inset-0 rounded-2xl opacity-50 mix-blend-overlay"
+        // style={style}
       >
-        <p className="absolute inset-x-0 text-xs h-full break-words whitespace-pre-wrap text-white font-mono font-bold transition duration-500">
+        <p className="absolute inset-x-0 h-full break-words whitespace-pre-wrap text-white font-medium transition duration-500">
           {randomString}
         </p>
       </motion.div>
