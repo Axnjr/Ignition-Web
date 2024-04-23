1e4146b05409a73c0dbefbea36c5510f9be92027
Yakshit Chhipa <yakshitchhipa@gmail.com>
2024-04-23 21:22:21 +0530

    fix: api routes

--- src/app/api/exec/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-import { EC2Client, RunInstancesCommand, DescribeAccountAttributesCommand } from "@aws-sdk/client-ec2";
-import { Client } from 'ssh2';
-
-export async function GET(req: NextRequest) {
-
-}
