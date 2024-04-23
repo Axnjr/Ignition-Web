d311a083e73e7c7904f260b144e5456b39dedbe8
Yakshit Chhipa <yakshitchhipa@gmail.com>
2024-04-23 21:22:38 +0530

    fix: dashbaord layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,5 @@
 import Sidebar from '../../../components/dashboard/Sidebar';
-import Button from '../../../components/home/Button';
+import { Button } from '../../../components/ui/Button';
 import Modes from '../../../components/home/Modes';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
@@ -10,8 +10,8 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             px-4 dark:border-neutral-800 bg-white text-black dark:text-white dark:bg-black
             justify-between flex-row-reverse'>
                 <div className='flex items-center gap-2'>
-                    <Button text='Docs' varient='plain' />
-                    <Button text='Feedback' varient="normal" />
+                    <Button variant="ghost">Docs</Button>
+                    <Button variant="ghost">Feedback</Button>
                     <Modes className="mx-4 text-black dark:text-white" />
                 </div>
             </header>
