9118e8d3e45c2f4a4c06ff938878849407360564
Yakshit Chhipa <yakshitchhipa@gmail.com>
2024-04-23 16:09:22 +0530

    refractor: components

--- components/constants.ts
+++ components/constants.ts
@@ -51,3 +51,122 @@ export const play: Play[] = [
     },
 ]
 
+export const syncCodeBlock = `import { useSync } from "@ignition/react";
+
+export default function MyComp() {
+    // data to be synced
+    const [state, setState, onExternalChange] = useSync({  
+        group: "site_viewers",
+        maxClients: 500,
+        data: "You can sync anytype of data !"
+    });
+
+    return ( 
+		<>  {/* Your Component */}  </> 
+	)
+}`
+
+export const theme = {
+	plain: {
+		color: "#fff",
+		backgroundColor: "#1111124c",
+	},
+	styles: [
+		{
+			types: ["prolog"],
+			style: {
+				color: "#ec5189",
+			},
+		},
+		{
+			types: ["comment"],
+			style: {
+				color: "rgb(50,50,50)",
+			},
+		},
+		{
+			types: ["builtin", "changed", "keyword", "interpolation-punctuation"],
+			style: {
+				color: "#2a6af3",
+			},
+		},
+		{
+			types: ["number", "inserted"],
+			style: {
+				color: "#32cba3",
+			},
+		},
+		{
+			types: ["constant"],
+			style: {
+				color: "#100ff2",
+			},
+		},
+		{
+			types: ["attr-name", "variable"],
+			style: {
+				color: "",
+			},
+		},
+		{
+			types: ["deleted", "string", "attr-value", "template-punctuation"],
+			style: {
+				color: "#E2AAFC",
+			},
+		},
+		{
+			types: ["selector"],
+			style: {
+				color: "",
+			},
+		},
+		{
+			// Fix tag color
+			types: ["tag"],
+			style: {
+				color: "rgb(78, 201, 176)",
+			},
+		},
+		{
+			// Fix tag color for HTML
+			types: ["tag"],
+			languages: ["markup"],
+			style: {
+				color: "rgb(86, 156, 214)",
+			},
+		},
+		{
+			types: ["punctuation", "operator"],
+			style: {
+				color: "rgb(212, 212, 212)",
+			},
+		},
+		{
+			// Fix punctuation color for HTML
+			types: ["punctuation"],
+			languages: ["markup"],
+			style: {
+				color: "#808080",
+			},
+		},
+		{
+			types: ["function"],
+			style: {
+				color: "#ec5189",
+			},
+		},
+		{
+			types: ["class-name"],
+			style: {
+				color: "rgb(78, 201, 176)",
+			},
+		},
+		{
+			types: ["char"],
+			style: {
+				color: "#100fed",
+			},
+		},
+	],
+}
+
